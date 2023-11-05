a51dcf6526f027193dcc0209c727701bed6bd25a
shrinithivijayaraghavan <109450620+shrinithivijayaraghavan@users.noreply.github.com>
2023-11-05 22:38:43 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './task-list/task-list.component';
+import { AddTaskComponent } from './add-task/add-task.component';
+import { TaskService } from './task.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListComponent,
+    AddTaskComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [TaskService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
